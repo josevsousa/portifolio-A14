a68ba774c3d93ce31de05ad1c797d10e1d891c0f
josevsousa <jose.vicente.de.sousa@gmail.com>
2023-02-09 17:04:52 -0300

    codigo busca-cep organizado

--- src/app/pages/projetos/buscar-cep/buscar-cep.component.ts
+++ src/app/pages/projetos/buscar-cep/buscar-cep.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { Validators, FormGroup, FormBuilder, NgForm } from '@angular/forms';
-import { stringify } from '@firebase/util';
-// import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 import { iBusca } from 'src/app/models/Busca';
-
 
 import { BuscaCepService } from 'src/app/services/busca-cep.service';
 
@@ -15,73 +11,59 @@ import { BuscaCepService } from 'src/app/services/busca-cep.service';
 })
 export class BuscarCepComponent {
 
-  
-
   inputCep: string = '';
   msgErro: string = "Error";
   validando: boolean = true;
   valido: boolean = false;
   loading: boolean = true;
-  cepEncontrado: iBusca = {
-    cep: "",
-    logradouro: "",
-    complemento: "",
-    bairro: "",
-    localidade: "",
-    uf: "",
-    unidade: "",
-    ibge: "",
-    gia: ""
+  cepEncontrado: iBusca; 
+
+  constructor( private buscaCepService: BuscaCepService){
+    this.cepEncontrado = {
+      cep: "",
+      logradouro: "",
+      complemento: "",
+      bairro: "",
+      localidade: "",
+      uf: "",
+      unidade: "",
+      ibge: "",
+      gia: ""
+    }
   }
 
-  maskValue: String | undefined;
-  binding: any;
-
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private buscaCepService: BuscaCepService
-
-  ){}
-
-
   ngKeyup(){
-
-    this.valido = false;
     this.validando = true;
-
+    
     this.loading = false;
-
     if(this.inputCep.length == 8){
-      this.validar();  
+      this.validar(); 
+
+    }else if(this.inputCep.length == 0){
+      this.loading = true;
     }
 
   } 
 
-
   validar(){
-     let dataObj: any = {erro: false};
-
+    let dataObj: any = {erro: false};
     //valida se tudo e numero
     this.buscaCepService.getCep(String(this.inputCep))
-      
-    .subscribe((data)=>{
+      .subscribe((data)=>{
 
-       dataObj = data;
+        dataObj = data;
 
-  if (!dataObj.erro) {
-        this.validando = true;
-        this.valido = true; 
-        // this.cepEncontrado = data;
-        this.cepEncontrado = dataObj;
-      } else {
-        this.validando = false;
-        this.msgErro = "cep errado";
-      }
-    this.loading = true;
-    });
+        if (!dataObj.erro) {
+              this.validando = true;
+              this.valido = true; 
+              this.cepEncontrado = dataObj;
+        }else {
+              this.validando = false;
+              this.msgErro = "cep errado";
+        }
+        
+        this.loading = true;
+      });
   }
   
-  
-
 }
